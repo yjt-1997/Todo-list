e53cfc8f30666a86eb31570d001a80bd0105c45d
yjt-1997 <jiangtap1997qqq@qq.com>
2019-07-21 11:16:40 +0800

    [YJT]display list

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <h2>Jquery To Do List</h2>
+    <p>
+      <em>Simple Todo List with adding and filter by diff status.</em>
+    </p>
+    <input type="text"><button>Add</button>
+    <List></List>
+    <a href="#">ALL</a>&nbsp;<a href="#">Active</a>&nbsp;<a href="#">Complete</a>
+  </div>
+</template>
+
+<script>
+import List from "./components/List";
+export default {
+  name: "app",
+  components: {
+    List
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <ol>
+      <li class>
+        <input name="todo-list" type="checkbox" class="todo-list" />
+        <span>123</span>
+      </li>
+    </ol>
+  </div>
+</template>
+<script>
+export default {
+  name: "List",
+  props: {
+    items: Array
+  }
+};
+</script>
+<style></style>
