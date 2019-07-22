e0dd16056c7957b33daf785987f01a2c1b2f04ba
YOUNG YANG (ITA-ISDC-ISD-OOCLL/ZHA) <YANGYO7@OOCL.COM>
2019-07-22 11:16:51 +0800

    rebuild code : add Header.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
-    <h2>Jquery To Do List</h2>
-    <p>
-      <em>Simple Todo List with adding and filter by diff status.</em>
-    </p>
+    <Header></Header>
     <input type="text" v-model="item" />
     <button @click="addItem">Add</button>
     <List :items="items"></List>
@@ -24,11 +21,13 @@
 </template>
 
 <script>
+import Header from "./components/Header";
 import List from "./components/List";
 export default {
   name: "app",
   components: {
-    List
+    List,
+    Header
   },
   data() {
     return {
