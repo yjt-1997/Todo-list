3a328a3308d73e8e6b731ef7174aec6d5996dc29
YOUNG YANG (ITA-ISDC-ISD-OOCLL/ZHA) <YANGYO7@OOCL.COM>
2019-07-22 16:28:02 +0800

    [YJT]add Item.vue

---
+++ src/components/Item.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <input name="todo-list" type="checkbox" v-model="item.isCompleted" />
+    <span>{{item.content}}</span>
+  </div>
+</template>
+<script>
+export default {
+  name: "Item",
+  props: {
+    item:Object
+  }
+};
+</script>
+<style></style>
--- src/components/List.vue
+++ src/components/List.vue
@@ -1,27 +1,21 @@
 <template>
   <div>
     <ol>
-      <li v-for="(item, index) in items" :key="index" v-bind:class="{checked:item.isCompleted}">
-        <input name="todo-list" type="checkbox" v-model="item.isCompleted" />
-        <span @dclick="updateItem(item,index)">{{item.content}}</span>
+      <li v-for="(item, index) in items" :key="index">
+        <Item :item="item"></Item>
       </li>
     </ol>
   </div>
 </template>
 <script>
+import Item from "./Item";
 export default {
   name: "List",
+  components: {
+    Item
+  },
   props: {
     items: Array
-  },
-  methods: {
-    updateItem(item, index) {
-      let str = prompt("请输入修改后的信息：");
-      if (str) {
-        item.content = str;
-        this.$emit("updateItem", item, index);
-      }
-    }
   }
 };
 </script>
