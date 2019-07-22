21a11bb5de9d5749c05b05ece7b021447031837d
YOUNG YANG (ITA-ISDC-ISD-OOCLL/ZHA) <YANGYO7@OOCL.COM>
2019-07-22 16:41:31 +0800

    [YJT]add Filters.vue

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,7 @@
     <Header></Header>
     <AddItem @toAddItem="addItem"></AddItem>
     <List :items="items"></List>
-    <div>
-      <ul id="filters">
-        <li>
-          <a href="#" @click="display('All')">ALL</a>
-        </li>
-        <li>
-          <a href="#" @click="display('Actived')">Active</a>
-        </li>
-        <li>
-          <a href="#" @click="display('Completed')">Complete</a>
-        </li>
-      </ul>
-    </div>
+    <Filters :items="itemsBackup" @clickFilter="display"></Filters>
   </div>
 </template>
 
@@ -23,12 +11,14 @@
 import Header from "./components/Header";
 import AddItem from "./components/AddItem";
 import List from "./components/List";
+import Filters from './components/Filters'
 export default {
   name: "app",
   components: {
     List,
     Header,
-    AddItem
+    AddItem,
+    Filters
   },
   data() {
     return {
@@ -41,14 +31,8 @@ export default {
       this.items.push(item);
       this.itemsBackup.push(item);
     },
-    display(flag) {
-      if (flag == "All") {
-        this.items = this.itemsBackup;
-      } else if (flag == "Actived") {
-        this.items = this.itemsBackup.filter(item => item.isCompleted == false);
-      } else {
-        this.items = this.itemsBackup.filter(item => item.isCompleted == true);
-      }
+    display(filterItems) {
+      this.items = filterItems;
     }
   }
 };
---
+++ src/components/Filters.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <ul id="filters">
+      <li>
+        <a href="#" @click="display('All')">ALL</a>
+      </li>
+      <li>
+        <a href="#" @click="display('Actived')">Active</a>
+      </li>
+      <li>
+        <a href="#" @click="display('Completed')">Complete</a>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "Filters",
+  props: {
+    items: Array
+  },
+  methods: {
+    display(flag) {
+      if (flag == "All") {
+        this.$emit("clickFilter", this.items);
+      } else if (flag == "Actived") {
+        this.$emit(
+          "clickFilter",
+          this.items.filter(item => item.isCompleted == false)
+        );
+      } else {
+        this.$emit(
+          "clickFilter",
+          this.items.filter(item => item.isCompleted == true)
+        );
+      }
+    }
+  }
+};
+</script>
+<style></style>
