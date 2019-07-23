b08b6398b7f8275b92fd219a3b6317c9853352ec
yjt-1997 <jiangtap1997qqq@qq.com>
2019-07-23 19:14:57 +0800

    [YJT]delete extra code

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -3,15 +3,6 @@ export const mutations = {
         state.items = newItems;
         state.toDisplayItems = newItems;
     },
-    addItem: (state, item) => {
-        item.id = state.items.length + 1;
-        state.items.push(item);
-        state.toDisplayItems.push(item);
-    },
-    update: (state, id) => {
-        let item = state.items.filter(item => item.id == id);
-        item.isCompleted = !item.isCompleted;
-    },
     updateDisplayItems: (state, flag) => {
         if (flag == "All") {
             state.toDisplayItems = state.items;
