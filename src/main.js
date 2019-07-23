06f4e726afa7047e8305c612b8a568c749a3b188
YOUNG YANG (ITA-ISDC-ISD-OOCLL/ZHA) <YANGYO7@OOCL.COM>
2019-07-23 14:26:08 +0800

    [YJT]add index.js

--- src/main.js
+++ src/main.js
@@ -1,36 +1,8 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
 import App from './App.vue'
+import { store } from './store/index'
 
-Vue.use(Vuex)
 Vue.config.productionTip = false
-
-const store = new Vuex.Store({
-    state: {
-        items: [],
-        toDisplayItems: [],
-    },
-    mutations: {
-        addItem(state, item) {
-            item.id = state.items.length + 1;
-            state.items.push(item);
-            state.toDisplayItems.push(item);
-        },
-        update(state, id) {
-            let item = state.items.filter(item => item.id == id);
-            item.isCompleted = !item.isCompleted;
-        },
-        updateDisplayItems(state, flag) {
-            if (flag == "All") {
-                state.toDisplayItems = state.items;
-            } else if (flag == "Actived") {
-                state.toDisplayItems = state.items.filter(item => !item.isCompleted);
-            } else {
-                state.toDisplayItems = state.items.filter(item => item.isCompleted);
-            }
-        }
-    }
-})
 
 new Vue({
     store,
---
+++ src/store/mutations.js
@@ -0,0 +1,20 @@
+export const mutations = {
+    addItem: (state, item) => {
+        item.id = state.items.length + 1;
+        state.items.push(item);
+        state.toDisplayItems.push(item);
+    },
+    update: (state, id) => {
+        let item = state.items.filter(item => item.id == id);
+        item.isCompleted = !item.isCompleted;
+    },
+    updateDisplayItems: (state, flag) => {
+        if (flag == "All") {
+            state.toDisplayItems = state.items;
+        } else if (flag == "Actived") {
+            state.toDisplayItems = state.items.filter(item => !item.isCompleted);
+        } else {
+            state.toDisplayItems = state.items.filter(item => item.isCompleted);
+        }
+    }
+}
